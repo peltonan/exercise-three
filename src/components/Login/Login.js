115597a139df412da949253abadd7f52df211024
peltonan <peltonen.annette@gmail.com>
2022-06-03 22:01:14 +0300

    useReducer - Section 10//Part 118

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
+  }
+  if (action.type === 'USER_BLUR') {
+    return { value: state.value, isValid: state.value.includes('@') };
+  }
+  return { value: '', isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  /*  const [enteredEmail, setEnteredEmail] = useState('');
+   const [emailIsValid, setEmailIsValid] = useState(); */
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: null,
+  });
 
   //"You must add all "things" you use in your effect function 
   //if those "things" could change because your component (or some parent component) 
@@ -28,31 +42,38 @@ const Login = (props) => {
   //useEffect runs when site is rendered, BUT entering enteredX in the array, a dependency, 
   //makes it check for changes, and ONLY then renders. The array [] stops "auto-render."//
 
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log('Checking from Validity!');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      );
-    }, 500);
-
-    return () => {
-      console.log('Cleanup');
-      clearTimeout(identifier);
-    };
-  }, [enteredEmail, enteredPassword]);
+  /*  useEffect(() => {
+     const identifier = setTimeout(() => {
+       console.log('Checking from Validity!');
+       setFormIsValid(
+         enteredEmail.includes('@') && enteredPassword.trim().length > 6
+       );
+     }, 500);
+ 
+     return () => {
+       console.log('Cleanup');
+       clearTimeout(identifier);
+     };
+   }, [enteredEmail, enteredPassword]); */
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({ type: 'INPUT_BLUR' });
   };
 
   const validatePasswordHandler = () => {
@@ -61,21 +82,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+          className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
             }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
